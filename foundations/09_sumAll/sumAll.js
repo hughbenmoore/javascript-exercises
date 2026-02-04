5103c2f2bcc351983e1024b8d4453a5a22376a1b
hughbenmoore <hughbenmoore@gmail.com>
2026-02-03 19:37:50 -0600

    Stuggled a lot with these exercises. Had to look at solution sometimes to understand what i was missing. Definetly got easier as I completed them.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+	let arraySum = 0;
+	for (let i = num1; i <= num2; i++) {
+		arraySum += i;
+	};
+	return arraySum;
 };
 
 // Do not edit below this line
--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let celsius = 5/9 * (num - 32);
+  let roundedC = Math.round(celsius * 10) / 10;
+  return roundedC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let fahrenheit = 9/5 * (num) + 32;
+  let roundedF = Math.round(fahrenheit * 10) / 10;
+  return roundedF;
 };
 
 // Do not edit below this line
